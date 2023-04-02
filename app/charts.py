f1b3f6d434c8c530f407161f61e583e561b17fb0
Ruben Gomez <gruben82@hotmail.com>
2023-04-02 16:47:08 -0500

    add docs

---
+++ app/charts.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+def generate_bar_chart(name, labels, values):
+  fig, ax = plt.subplots()
+  ax.bar(labels, values)
+  plt.savefig(f'./imags/{name}.png')
+  plt.close()
+  
+def generate_pie_chart(labels, values):
+  fig, ax = plt.subplots()
+  ax.pie(values, labels=labels)
+  plt.savefig('pie.png')
+  plt.close()
+
+if __name__ == '__main__':
+  labels = ['a', 'b', 'c']
+  values = [10, 40, 100]
+  # generate_bar_chart(labels, values)
+  generate_pie_chart(labels, values)
+  
+    
+
+
